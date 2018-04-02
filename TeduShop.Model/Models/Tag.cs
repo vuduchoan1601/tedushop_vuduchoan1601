f215a53fa5be3af6013187d637afc10119c0b627
HoanGi-PC\HoanGi <vuduchoan1601@gmail.com>
2018-04-02 23:06:47 +0700

    add class Tags

---
+++ TeduShop.Model/Models/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        [Required]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        public string Name { set; get; }
+
+        public string Type { set; get; }
+    }
+}
