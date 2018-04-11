20cfc2d9c7141685ed6ccf387a5b076438734ab9
HoanGi-PC\HoanGi <vuduchoan1601@gmail.com>
2018-04-11 23:25:49 +0700

    commit more service

--- TeduShop.Data/Repositories/PostRepository.cs
+++ TeduShop.Data/Repositories/PostRepository.cs
@@ -1,16 +1,36 @@
-﻿using TeduShop.Data.Infrastructure;
+﻿using System;
+using System.Collections.Generic;
+using TeduShop.Data.Infrastructure;
 using TeduShop.Model.Models;
+using System.Linq;
 
 namespace TeduShop.Data.Repositories
 {
     public interface IPostRepository : IRepository<Post>
     {
+        IEnumerable<Post> GetAllByTag(string tag, int pagIndex, int pageSize, out int totalRow);
     }
 
-    public class PostRepository : RepositoryBase<Post>
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
     {
         public PostRepository(IDbFactory dbFactory) : base(dbFactory)
         {
+        }
+
+        public IEnumerable<Post> GetAllByTag(string tag, int pagIndex, int pageSize, out int totalRow)
+        {
+            var query = from p in DbContext.Posts
+                        join pt in DbContext.PostTags
+                        on p.ID equals pt.PostID
+                        where pt.TagID == tag && p.Status == true
+                        orderby p.CreateDate descending
+                        select p;
+
+            totalRow = query.Count();
+
+            query = query.Skip((pagIndex - 1) * pageSize).Take(pageSize);
+
+            return query;
         }
     }
 }
